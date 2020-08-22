7f89bc5b731bed794b8dc9860748b23c90006218
148921 <comtruice@gmail.com>
2020-08-22 22:21:05 +0200

    Get access_token and fetch all mounts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    var token = "";
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("grant_type", "client_credentials");
+    var requestOptions = {
+      headers: {
+        Authorization: `Basic ${token}`,
+      },
+      method: "POST",
+      body: urlencoded,
+      redirect: "follow",
+    };
+
+    fetch("https://us.battle.net/oauth/token", requestOptions)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        const access_token = json.access_token;
+        const url = `https://us.api.blizzard.com/data/wow/mount/index?namespace=static-us&locale=en_US&access_token=${access_token}`;
+        fetch(url)
+          .then((res) => res.json())
+          .then((json) => {
+            console.log(json);
+          });
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Oppsett fungerer
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>TEST</h1>
     </div>
   );
-}
+};
 
 export default App;
